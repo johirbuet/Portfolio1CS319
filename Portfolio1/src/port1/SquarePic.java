2a0ae3257fb004dc1d2c1de0ad708144cac56023
Owner <Owner@64.113.95.199>
2015-01-21 16:53:51 -0600

    start

---
+++ Portfolio1/src/port1/SquarePic.java
@@ -0,0 +1,16 @@
+package port1;
+
+import java.util.*;
+import javax.swing.*;
+
+public class SquarePic 
+{
+	private Coordinate begin, current;
+	
+	public SquarePic(Coordinate begin, Coordinate current)
+	{
+		this.begin = begin;
+		this.current = current;
+	}
+
+}
